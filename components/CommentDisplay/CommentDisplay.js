95a3e16d2547aa047c0f2165b811117c28d2160b
latz09 <85132328+latz09@users.noreply.github.com>
2023-02-22 14:36:31 -0600

    d

--- components/CommentDisplay/CommentDisplay.js
+++ components/CommentDisplay/CommentDisplay.js
@@ -3,7 +3,7 @@ import { BsHandThumbsUp, BsHandThumbsDown } from 'react-icons/bs';
 const CommentDisplay = ({ discussion }) => {
 	console.log(discussion);
 	return (
-		<div className='h-[85vh] flex flex-col gap-8 justify-between '>
+		<div className='h-[80vh] flex flex-col gap-8 justify-between '>
 			<div className='grid  h-full place-items-start  overflow-y-scroll scrollbar-hide rounded-lg '>
 				<div className='grid gap-8 p-8 '>
 					{discussion.map((comment, index) => (
@@ -29,7 +29,7 @@ const Comment = ({ comment, userId, upvotes, downvotes }) => {
 	return (
 		<div className=' grid'>
 			<span className='text-xl lg:text-2xl font-semibold text-primary'>user {userId}</span>
-			<span className='text-lg lg:text-xl font-semibold tracking-wider text-light/70 '>
+			<span className='text-lg lg:text-xl lg:font-semibold tracking-wider text-light/70 '>
 				{comment}
 			</span>
 			<div className=" place-self-end">
@@ -41,15 +41,15 @@ const Comment = ({ comment, userId, upvotes, downvotes }) => {
 
 const CreateComment = () => {
 	return (
-		<div className=' w-full p-2   lg:flex justify-between gap-4'>
+		<div className=' w-full p-2   lg:flex justify-between gap-4 items-center'>
 			<div className=' flex-grow '>
 				<input
 					type='text'
 					placeholder='Add a comment'
-					className='w-full h-full px-4 py-2 bg-triary/10 focus'
+					className='w-full h-full p-4 bg-triary/10 focus'
 				/>
 			</div>
-			<div className="text-center mt-4">
+			<div className="text-center ">
 				<button className='btn bg-primary'>Send it</button>
 			</div>
 		</div>
--- components/utils/ModalOverlay.js
+++ components/utils/ModalOverlay.js
@@ -1,4 +1,4 @@
-// import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const ModalOverlay = ({ children, setModalIsOpen, topic }) => {
 	
@@ -19,8 +19,8 @@ const ModalOverlay = ({ children, setModalIsOpen, topic }) => {
 	};
 
 	return (
-		// <AnimatePresence mode='wait'>
-		<div
+		<AnimatePresence >
+		<motion.div
 			variants={backDropVariant}
 			initial='hidden'
 			animate='visible'
@@ -28,7 +28,7 @@ const ModalOverlay = ({ children, setModalIsOpen, topic }) => {
 			className='fixed top-0 left-0 w-full h-full   text-light bg-dark/90 backdrop-blur-md '
 		>
 			<div className=' flex flex-col max-w-4xl mx-auto  '>
-				<div className='grid place-items-center lg:flex justify-between items-center text-lg lg:text-3xl tracking-wider h-[10vh] mx-4'>
+				<div className='grid place-items-center lg:flex justify-between items-center text-lg lg:text-3xl tracking-wider h-[15vh] mx-4'>
 					<div className="italic opacity-70 text-primary font-semibold">{topic}</div>
 					<div
 						className=' text-white  cursor-pointer  '
@@ -41,8 +41,8 @@ const ModalOverlay = ({ children, setModalIsOpen, topic }) => {
 				</div>
 				<div className='h-full '>{children}</div>
 			</div>
-		</div>
-		// </AnimatePresence>
+		</motion.div>
+		</AnimatePresence>
 	);
 };
 
