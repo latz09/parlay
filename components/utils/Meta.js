74d84f29feb631e5bacd209c8db751cdd79037f3
latz09 <85132328+latz09@users.noreply.github.com>
2023-03-09 19:12:30 -0600

    og:image

--- components/utils/Meta.js
+++ components/utils/Meta.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
-
+import quickLogo from '../../public/images/quick-logo.png'
 const Meta = ({ pageTitle, description, keywords }) => {
 	return (
 		<Head>
 			<title>{pageTitle}</title>
 			<meta name='description' content={description} />
 			<meta name="keywords" content={keywords} />
+            <meta property="og:image" content={quickLogo} />
 		</Head>
 	);
 };
