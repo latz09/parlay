22ff969e0b7a30d8c38c9587c416f6f247d573b3
latz09 <85132328+latz09@users.noreply.github.com>
2023-03-09 19:21:24 -0600

    1tagain

--- components/utils/Meta.js
+++ components/utils/Meta.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
-import quickLogo from '../../public/images/quick-logo.png'
+
 const Meta = ({ pageTitle, description, keywords }) => {
 	return (
 		<Head>
 			<title>{pageTitle}</title>
 			<meta name='description' content={description} />
 			<meta name="keywords" content={keywords} />
-            <meta property="og:image" content={quickLogo} />
+            <meta property="og:image" content='../../public/images/quick-logo.png' />
 		</Head>
 	);
 };
